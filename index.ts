dd9e81a718a0a260a122d7f34e720178a30f6f53
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-10-06 19:39:18 +0300

    8 uzd

--- index.ts
+++ index.ts
@@ -1,43 +1,42 @@
 class Trikampis {
-        a: number;
-        b: number;
-        c:number;
+    a: number;
+    b: number;
+    c: number;
 
-        public constructor (a: number, b: number, c: number) {
-            this.a = a;
-            this.b = b;
-            this.c= c;
-        }
-
- public spausdintiDuomenis(): void {
-            console.log(`Krastine a: ${this.a}`);
-            console.log(`Krastine b: ${this.b}`);
-            console.log(`Krastine c: ${this.c}`);
-            console.log(`Perimetras ${this.apskaiciuotiPerimetra()}`)
-            console.log(`Egzistuoja? ${this.trikampioPatikrinimas()}`)
+    public constructor(a: number, b: number, c: number) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
-        public apskaiciuotiPerimetra(): number{
-            return this.a + this.b + this.c
+    public spausdintiDuomenis(): void {
+        console.log(`Krastine a: ${this.a}`);
+        console.log(`Krastine b: ${this.b}`);
+        console.log(`Krastine c: ${this.c}`);
+        console.log(`Perimetras ${this.apskaiciuotiPerimetra()}`)
+        console.log(`Egzistuoja? ${this.trikampioPatikrinimas()}`)
     }
 
-        public trikampioPatikrinimas():boolean {
-            if(this.a + this.b > this.c && this.a + this.c > this.b && this.b + this.c > this.a) {
+    public apskaiciuotiPerimetra(): number {
+        return this.a + this.b + this.c
+    }
+
+    public trikampioPatikrinimas(): boolean {
+        if (this.a + this.b > this.c && this.a + this.c > this.b && this.b + this.c > this.a) {
             return true;
-        }
-            else return false;
-        } 
+        } else return false;
+    }
 
 }
 
-const trikampis1 = new Trikampis(5,6,7);
-const trikampis2= new Trikampis(7,3,100);
+const trikampis1 = new Trikampis(5, 6, 7);
+const trikampis2 = new Trikampis(7, 3, 100);
 
 console.log(trikampis1.spausdintiDuomenis());
 console.log(trikampis2.spausdintiDuomenis());
 
 
-    /**
+/**
  * 1. Susikurkite TypeScript projektą
  *
  * 2. Parašykite programą, kuri padeda dirbti su trikampiais

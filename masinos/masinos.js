ae7214707896fc2e99a89da65e9d62badadae8f5
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-10-14 23:08:01 +0300

    stuck at edit event listener

--- masinos/masinos.js
+++ masinos/masinos.js
@@ -1,32 +1,60 @@
 "use strict";
-// enum fuelTypes {
-//     benzinas = "Benzinas",
-//         dyzelinas = "Dyzelinas",
-// }
+var fuelTypes;
+(function (fuelTypes) {
+    fuelTypes["benzinas"] = "benzinas";
+    fuelTypes["dyzelinas"] = "dyzelinas";
+})(fuelTypes || (fuelTypes = {}));
+var formSaveDOM = document.getElementById('add_car');
+var formUpdateDOM = document.getElementById('update_car');
+var listDOM = document.getElementById('list');
+function renderAddForm() {
+    return formSaveDOM.innerHTML = "<form>\n                <input id=\"model\" type=\"text\" placeholder=\"Modelis\">\n                <input id=\"date\" type=\"date\" placeholder=\"Pagaminimo data\">\n                <input id=\"color\" type=\"text\" placeholder=\"Spalva\">\n                <input id=\"fuel\" list=\"fuelList\" name=\"fuelTypes\" placeholder=\"Kuro tipas\">\n                <datalist id=\"fuelList\">\n                    <option value=\"" + fuelTypes.benzinas + "\">\n                    <option value=\"" + fuelTypes.dyzelinas + "\">\n                </datalist>\n\n                <button id=\"save\" type=\"button\">Prideti</button>\n            </form>";
+}
+function renderUpdateForm() {
+    return formUpdateDOM.innerHTML = " <form>\n                <input id=\"model\" type=\"text\" placeholder=\"Modelis\">\n                <input id=\"date\" type=\"date\" placeholder=\"Pagaminimo data\">\n                <input id=\"color\" type=\"text\" placeholder=\"Spalva\">\n                <input id=\"fuel\" list=\"fuel\" name=\"fuelTypes\" placeholder=\"Kuro tipas\">\n                <datalist id=\"fuelTypes\">\n                    <option value=\"" + fuelTypes.benzinas + "\">\n                    <option value=\"" + fuelTypes.dyzelinas + "\">\n                </datalist>\n\n                <button class=\"save\" type=\"button\">Atnaujinti</button>\n            </form>";
+}
+renderAddForm();
+renderUpdateForm();
+function display() {
+    listDOM.innerHTML = "";
+    for (var _i = 0, cars_1 = cars; _i < cars_1.length; _i++) {
+        var car = cars_1[_i];
+        car.printEntry(listDOM);
+    }
+    var imgDOM = listDOM.querySelector(".entry .actions .edit");
+    var deleteDOM = listDOM.querySelector(".entry .actions .delete");
+    console.log(imgDOM);
+    console.log(deleteDOM);
+}
+var DOMs = {
+    modelInput: document.getElementById('model'),
+    dateInput: document.getElementById('date'),
+    colorInput: document.getElementById('color'),
+    fuelInput: document.getElementById('fuel'),
+    saveFormButton: formSaveDOM.querySelector('button'),
+    editFormButton: formUpdateDOM.querySelector('button'),
+};
 var Cars = /** @class */ (function () {
-    function Cars(model, date, color, fuel) {
+    function Cars(model, date, color, fuel, id) {
+        if (fuel === void 0) { fuel = fuelTypes.benzinas; }
         this.model = model;
         this.date = date;
         this.color = color;
         this.fuel = fuel;
+        this.id = id || Math.round(Math.random() * 10000);
     }
+    Cars.prototype.generateID = function () {
+        return this.id;
+    };
     Cars.prototype.printEntry = function (element) {
         if (element) {
-            element.innerHTML += "<div id=\"ivestis_id\" class=\"entry\">\n                <div class=\"entry_parameter\">" + this.model + "</div>\n                <div class=\"entry_parameter\">" + this.date + "</div>\n                <div class=\"entry_parameter\">" + this.color + "</div>\n                <div class=\"entry_parameter\">" + this.fuel + "</div>\n                <div class=\"actions\">\n                    <img id=\"edit\" src=\"./edit.png\" alt=\"Atnaujinti\">\n                    <img id=\"delete\" src=\"./delete.png\" alt=\"Istrinti\">\n                </div>\n            </div>";
+            element.innerHTML += "<div id=\"" + this.generateID() + "\" class=\"entry\">\n                <div class=\"entry_parameter\">" + this.model + "</div>\n                <div class=\"entry_parameter\">" + this.date + "</div>\n                <div class=\"entry_parameter\">" + this.color + "</div>\n                <div class=\"entry_parameter\">" + this.fuel + "</div>\n                <div class=\"actions\">\n                    <img class=\"edit\" src=\"./edit.png\" alt=\"Atnaujinti\">\n                    <img class=\"delete\" src=\"./delete.png\" alt=\"Istrinti\">\n                </div>\n            </div>";
         }
     };
     return Cars;
 }());
 var cars = [];
-var DOMs = {
-    listDOM: document.getElementById('list'),
-    modelInput: document.getElementById('model'),
-    dateInput: document.getElementById('date'),
-    colorInput: document.getElementById('color'),
-    fuelInput: document.getElementById('fuel'),
-    saveButton: document.getElementById('save'),
-};
-DOMs.saveButton.addEventListener("click", function () {
+DOMs.saveFormButton.addEventListener("click", function () {
     var model = DOMs.modelInput.value;
     var date = DOMs.dateInput.value;
     var color = DOMs.colorInput.value;
@@ -35,12 +63,11 @@ DOMs.saveButton.addEventListener("click", function () {
     cars.push(newCar);
     console.log(cars);
     display();
+    renderAddForm();
 });
-function display() {
-    DOMs.listDOM.innerHTML = "";
-    for (var _i = 0, cars_1 = cars; _i < cars_1.length; _i++) {
-        var car = cars_1[_i];
-        car.printEntry(DOMs.listDOM);
-    }
-    console.log("display");
-}
+imgDOM.addEventListener("click", function () {
+    formSaveDOM.classList.add('hide');
+    formUpdateDOM.classList.remove('hide');
+});
+DOMs.editFormButton.addEventListener("click", function () {
+});
--- masinos/masinos.ts
+++ masinos/masinos.ts
@@ -1,72 +1,124 @@
-// enum fuelTypes {
-//     benzinas = "Benzinas",
-//         dyzelinas = "Dyzelinas",
-// }
+ enum fuelTypes {
+     benzinas = "benzinas",
+         dyzelinas = "dyzelinas",
+ }
 
-class Cars {
-    public readonly model: string;
-    public readonly date: string;
-    public readonly color: string;
-    public readonly fuel: string;
+ const formSaveDOM = document.getElementById('add_car') as HTMLElement
+ const formUpdateDOM = document.getElementById('update_car') as HTMLElement
+ const listDOM = document.getElementById('list') as HTMLElement
 
-    constructor(model: string, date: string, color: string, fuel: string) {
-        this.model = model;
-        this.date = date;
-        this.color = color;
-        this.fuel = fuel;
-    }
+ function renderAddForm(): string {
+     return formSaveDOM.innerHTML = `<form>
+                <input id="model" type="text" placeholder="Modelis">
+                <input id="date" type="date" placeholder="Pagaminimo data">
+                <input id="color" type="text" placeholder="Spalva">
+                <input id="fuel" list="fuelList" name="fuelTypes" placeholder="Kuro tipas">
+                <datalist id="fuelList">
+                    <option value="${fuelTypes.benzinas}">
+                    <option value="${fuelTypes.dyzelinas}">
+                </datalist>
 
-    public printEntry(element ? : HTMLElement): void {
-        if (element) {
-            element.innerHTML += `<div id="ivestis_id" class="entry">
+                <button id="save" type="button">Prideti</button>
+            </form>`
+ }
+
+ function renderUpdateForm(): string {
+     return formUpdateDOM.innerHTML = ` <form>
+                <input id="model" type="text" placeholder="Modelis">
+                <input id="date" type="date" placeholder="Pagaminimo data">
+                <input id="color" type="text" placeholder="Spalva">
+                <input id="fuel" list="fuel" name="fuelTypes" placeholder="Kuro tipas">
+                <datalist id="fuelTypes">
+                    <option value="${fuelTypes.benzinas}">
+                    <option value="${fuelTypes.dyzelinas}">
+                </datalist>
+
+                <button class="save" type="button">Atnaujinti</button>
+            </form>`
+ }
+
+ renderAddForm()
+ renderUpdateForm()
+
+ function display(): void {
+     listDOM.innerHTML = "";
+     for (const car of cars) {
+         car.printEntry(listDOM)
+     }
+     const imgDOM = listDOM.querySelector(".entry .actions .edit")
+     const deleteDOM = listDOM.querySelector(".entry .actions .delete")
+     console.log(imgDOM);
+     console.log(deleteDOM);
+ }
+
+ const DOMs = {
+     modelInput: document.getElementById('model') as HTMLInputElement,
+     dateInput: document.getElementById('date') as HTMLInputElement,
+     colorInput: document.getElementById('color') as HTMLInputElement,
+     fuelInput: document.getElementById('fuel') as HTMLInputElement,
+     saveFormButton: formSaveDOM.querySelector('button') as HTMLElement,
+     editFormButton: formUpdateDOM.querySelector('button') as HTMLElement,
+ }
+
+ class Cars {
+     public readonly model: string;
+     public readonly date: string;
+     public readonly color: string;
+     public readonly fuel: fuelTypes;
+     private id: number;
+
+     constructor(model: string, date: string, color: string, fuel: fuelTypes = fuelTypes.benzinas, id ? : number) {
+         this.model = model;
+         this.date = date;
+         this.color = color;
+         this.fuel = fuel;
+         this.id = id || Math.round(Math.random() * 10000);
+     }
+
+     public generateID(): number {
+         return this.id
+     }
+
+     public printEntry(element ? : HTMLElement): void {
+         if (element) {
+             element.innerHTML += `<div id="${this.generateID()}" class="entry">
                 <div class="entry_parameter">${this.model}</div>
                 <div class="entry_parameter">${this.date}</div>
                 <div class="entry_parameter">${this.color}</div>
                 <div class="entry_parameter">${this.fuel}</div>
                 <div class="actions">
-                    <img id="edit" src="./edit.png" alt="Atnaujinti">
-                    <img id="delete" src="./delete.png" alt="Istrinti">
+                    <img class="edit" src="./edit.png" alt="Atnaujinti">
+                    <img class="delete" src="./delete.png" alt="Istrinti">
                 </div>
             </div>`
-        }
-    }
-}
+         }
+     }
+ }
 
-let cars: Cars[] = [];
+ let cars: Cars[] = [];
 
-const DOMs = {
-    listDOM: document.getElementById('list')as HTMLElement,
-    modelInput: document.getElementById('model') as HTMLInputElement,
-    dateInput: document.getElementById('date') as HTMLInputElement,
-    colorInput: document.getElementById('color') as HTMLInputElement,
-    fuelInput: document.getElementById('fuel') as HTMLInputElement,
-    saveButton: document.getElementById('save') as HTMLElement,
-}
+ DOMs.saveFormButton.addEventListener("click", () => {
+     const model = DOMs.modelInput.value;
+     const date = DOMs.dateInput.value;
+     const color = DOMs.colorInput.value;
+     const fuel = DOMs.fuelInput.value;
 
+     const newCar = new Cars(model, date, color, fuel)
 
+     cars.push(newCar)
+     console.log(cars);
 
-DOMs.saveButton.addEventListener("click", () => {
-    const model = DOMs.modelInput.value;
-    const date = DOMs.dateInput.value;
-    const color = DOMs.colorInput.value;
-    const fuel = DOMs.fuelInput.value;
+     display()
+     renderAddForm()
+ })
 
-    const newCar = new Cars(model, date, color, fuel)
+ imgDOM.addEventListener("click", () => {
+     formSaveDOM.classList.add('hide');
+     formUpdateDOM.classList.remove('hide')
+ })
 
-    cars.push(newCar)
-console.log(cars);
+ 
 
-    display()
+ DOMs.editFormButton.addEventListener("click", () => {
 
-
-})
-
-
-function display(): void {
-    DOMs.listDOM.innerHTML = "";
-    for (const car of cars) {
-        car.printEntry(DOMs.listDOM)
-    }
-    console.log("display");
-    
-}
+ })
